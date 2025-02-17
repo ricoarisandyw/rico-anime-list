c1624e418bbde85b61b7f1d89c7e04151c83c485
ricoarisandyw <arisandyrico@gmail.com>
2025-02-17 16:58:19 +0700

    feat: use uploadthing

--- apps/web/app/actions/file-uploader.ts
+++ apps/web/app/actions/file-uploader.ts
@@ -1,5 +1,12 @@
 "use server";
 
+import { UTApi } from "uploadthing/server";
+
+const utapi = new UTApi({
+  token: process.env.UPLOADTHING_TOKEN,
+  logLevel: "Debug",
+});
+
 
 export async function uploadFile(formData: FormData) {
   try {
@@ -9,10 +16,13 @@ export async function uploadFile(formData: FormData) {
       throw new Error("No file uploaded");
     }
 
+    const result = await utapi.uploadFiles([file]);
+
     return {
       success: true,
       filename: file.name,
       size: file.size,
+      url: result.at(0)?.data,
     };
   } catch (error) {
     console.error(error);
